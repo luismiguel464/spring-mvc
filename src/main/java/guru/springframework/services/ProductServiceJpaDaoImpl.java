a4ed3d8d39ce830c61dc45852e551c249360a494
luisortiz <luismiguel464@gmail.com>
2018-11-12 22:34:48 -0500

    JPA: Added DAO Service implementation

---
+++ src/main/java/guru/springframework/services/ProductServiceJpaDaoImpl.java
@@ -0,0 +1,61 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Product;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+@Service
+@Profile("jpadao") //this is active on application.properties
+public class ProductServiceJpaDaoImpl implements ProductService {
+
+    private EntityManagerFactory emf; //EntityManagerFactory is thread safe while the EntityManager is not
+
+    @PersistenceUnit
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public List<Product> listAll() {
+        EntityManager em = emf.createEntityManager();
+
+        return em.createQuery("from Product", Product.class).getResultList();
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        EntityManager em = emf.createEntityManager();
+
+        return em.find(Product.class, id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product product) {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Product savedProduct = em.merge(product); //Merge creates a new object if object doesn't exist and merge the
+                                                    //properties of that object if it does exist.
+        em.getTransaction().commit();
+
+        return savedProduct;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.remove(em.find(Product.class, id));
+
+        em.getTransaction().commit();
+    }
+
+}
